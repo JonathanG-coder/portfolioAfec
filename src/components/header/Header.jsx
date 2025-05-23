34d90c40c6a5badb8cb7a22486c766d0133a5590
Jonathan <jonathangager75@gmail.com>
2025-05-23 10:34:01 +0200

    Add Localstorage to DarkMode

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,22 +4,32 @@ import "./Header.css";
 import logo from "../../assets/photo-profil.png";
 
 export default function Header() {
-  
   // Variable d'état ---
   // Le dark mode est activé par défaut (true)
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme === "light" ? false : true;
+  });
   const [open, setOpen] = useState(false);
 
   // Fonction pour inverser ---
   const toggleMenu = () => setOpen(!open);
-  const toggleDarkMode = () => setDarkMode(!darkMode);
+  // Fonction pour inverser ---
+const toggleDarkMode = () => {
+  const newMode = !darkMode;
+  setDarkMode(newMode);
+  localStorage.setItem("theme", newMode ? "dark" : "light");
+};
 
   // A chaque fois que DarkMode change, utilisaion de useEffect
-  useEffect(() => {     //() => Fonction fléchée, callback
+  useEffect(() => {
+    //() => Fonction fléchée, callback
     // Fonction pour changer le thème
     if (darkMode) {
       document.body.classList.add("dark-mode");
+      document.body.classList.remove("light-mode");
     } else {
+      document.body.classList.add("light-mode");
       document.body.classList.remove("dark-mode");
     }
   }, [darkMode]); //  = Tableau de dépendance qui surveille la liste des variables. CEtte fonction s'exécute à chaque fois que darkMode change
